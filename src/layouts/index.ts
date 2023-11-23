565460ad3c251c164a9a2f83882424d86c2e625f
Lucas Mitchel <lucasmitchelcampos17@gmail.com>
2023-11-23 13:38:54 -0300

    feat: Create CTA Layout

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as DefaultLayout } from './DefaultLayout.astro';
 export { default as SectionsLayout } from './SectionsLayout.astro';
 export { default as TextImageSectionLayout } from './TextImageSectionLayout.astro';
+export { default as CTASection } from './CTASectionLayout.astro';
